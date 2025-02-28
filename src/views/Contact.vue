f69f2134d5b70ce347cb790f9bebd60c910105a8
Rouli <nittram36@gmail.com>
2025-02-28 12:14:45 +0100

    Improve contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,10 +2,52 @@
   <div>
     
     <h1>Let's get in touch</h1>
+    <ul>
+      <li>
+        <a target="_blank" href="mailto:martin.loiseau49@gmail.com">
+          <img title="Send me an email" src="img/icons/mail.png">
+          Email - martin.loiseau49@gmail.com
+        </a>
+      </li>
 
-    <!--
+      <li>
+        <a target="_blank" href="https://www.linkedin.com/in/martin-loiseau-ab3b76193/">
+          <img title="Send me an email" src="img/icons/linkedin.png">
+          LinkedIn - Martin Loiseau
+        </a>
+      </li>
+
+      <li>
+        <a target="_blank" href="https://github.com/Rouli-M/">
+          <img title="Find me on Github" src="img/icons/github.png">
+          GitHub - Rouli-M
+        </a>
+      </li>
+
+      <li>
+        <a target="_blank" href="https://www.instagram.com/rouli.overflow/">
+          <img title="Find me on Instagram" src="img/icons/instagram.png">
+          Instagram - @rouli.overflow
+        </a>
+      </li>
+
+      <li>
+        <a target="_blank" href="https://bsky.app/profile/rouli-overflow.bsky.social">
+          <img title="Find me on Bluesky" src="img/icons/bluesky.png">
+          Bluesky - @rouli-overflow
+        </a>
+      </li>
+
+      <li>
+        <a target="_blank" href="https://rouli.itch.io">
+          <img title="Find me on Bluesky" src="img/icons/itch.png">
+          Itch.io - rouli.itch.io
+        </a>
+      </li>
+
+    </ul>
+      <!--
     <div style="margin-bottom:40px;">Feel free to contact me about any opportunity, or just to chat.</div>
--->
     <ul>
       <li>
         <i class="fa fa-envelope-o fa-lg fa-fw"></i>
@@ -28,22 +70,50 @@
         <a href="https://bsky.app/profile/rouli-overflow.bsky.social" target="_blank">blueksy</a>
       </li>
     </ul>
+    -->
 
   </div>
 </template>
 
 <style scoped>
-ul {
-  list-style: none;
-  padding-inline-start: 0px;
+img {
+   filter: invert(19%) sepia(26%) saturate(5434%) hue-rotate(260deg) brightness(93%) contrast(96%);
+  opacity:1;
+  transform: rotate(0deg);
+   /*  border: 1px solid red; Temporary border to visualize images */
 }
 
-li {
-    line-height: 1.8em;
-    font-size: 1.2em;
-  }
+ul {
+  list-style: none;
+  padding-inline-start: 0; /* Remove default padding for ul */
+}
 
-@media only screen and (min-width: 620px){
+li a {
+  display: flex; /* Use Flexbox for the list items */
+  align-items: center; /* Center items vertically */
+  gap: 20px; /* Add space between image and text */
+  line-height: 1.8em;
+  font-size: 1.2em;
+  margin-bottom: 15px;
+}
+
+/* Define the keyframe animation */
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(20deg);
+  }
+}
+
+/* Apply the animation on hover */
+li:hover img {
+  animation: rotate 0.1s linear forwards;
+}
+
+
+@media only screen and (min-width: 620px) {
   ul {
     padding-inline-start: 40px;
   }
@@ -57,4 +127,5 @@ li {
 i {
   margin-right: 20px;
 }
+
 </style>
